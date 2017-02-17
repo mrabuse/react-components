a3b2ea0f96f1779d1db5efb7b13b94b08bfd4e4c
mrabuse <cogglemat@gmail.com>
2017-02-17 12:40:18 -0600

    Add bold hover event

--- app.js
+++ app.js
@@ -2,22 +2,34 @@ const GroceryList = () => (
     <ul>
       {GroceryListItems()}
     </ul>
-);
+)
 
 class GroceryListItem extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
+
+    this.state = {
+      done: false
+    }
+  }
+
+  onListItemHover () {
+    this.setState({
+      done: !this.state.done
+    });
   }
 
   render () {
+
+    let style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+
     return (
-      <li>{this.props.item}</li>
+      <li style={style} onMouseEnter={this.onListItemHover.bind(this)} onMouseLeave={this.onListItemHover.bind(this)}>{this.props.item}</li>
     )
   }
 }
-
-
-
 
 function GroceryListItems () {
   const groceries = ['Cucumbers', 'Kale'];
