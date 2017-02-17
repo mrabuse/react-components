fe2493619a4a153cb63c653b2eede65d1cbf0c51
mrabuse <cogglemat@gmail.com>
2017-02-17 11:54:19 -0600

    Create and render  GroceryList ul  w/ 2 li

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// TODO
+const GroceryList = () => (
+    <ul>
+      <li />
+      <li />
+    </ul>
+);
+
+const
+// function GroceryListItem (props) {
+//   return <li>{props.item}</li>;
+// }
+//
+//
+// function GroceryListItems () {
+//   const groceries = ['Cucumbers', 'Kale'];
+//
+//   return groceries.map((item) => <GroceryListItem key={item} item={item} />)
+// }
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
